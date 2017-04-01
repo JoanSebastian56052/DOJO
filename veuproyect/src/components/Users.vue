c5f858eb2156c3a6b495af15d3ee82b6718c861e
joansebastian56052 <chicoriolds@gmail.com>
2017-04-01 09:48:36 -0500

    Creacion DOJO vue

---
+++ veuproyect/src/components/Users.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  	<h1>View Users</h1>
+  	<ul v-for="user in users">
+  		<li>
+  			{{user.name}}
+  		</li>
+  	</ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+  	return {
+  		users: []
+  	}
+  },
+
+  mounted() {
+  	axios.get("https://jsonplaceholder.typicode.com/users")
+  	.then(response => this.users = response.data)
+  }
+}
+</script>
+<style>
+
+</style>
